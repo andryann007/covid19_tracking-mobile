ebf73e3963a1268b9783cd4226df5a616b687006
Andryan <andryancoolz@gmail.cpm>
2022-12-20 23:44:17 +0700

    COVID-19 Tracking Apps using disease.sh API

--- app/src/main/java/com/example/covid19tracking/activities/ContinentDetailActivity.java
+++ app/src/main/java/com/example/covid19tracking/activities/ContinentDetailActivity.java
@@ -1,20 +1,28 @@
 package com.example.covid19tracking.activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.text.HtmlCompat;
 
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
 import com.example.covid19tracking.R;
 import com.example.covid19tracking.api.ApiClient;
 import com.example.covid19tracking.api.ApiService;
@@ -22,10 +30,8 @@ import com.example.covid19tracking.api.ContinentResult;
 import com.example.covid19tracking.databinding.ActivityContinentDetailBinding;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 import retrofit2.Call;
@@ -41,7 +47,11 @@ public class ContinentDetailActivity extends AppCompatActivity {
 
     private String continent;
 
-    private AnyChartView anyChartView;
+    private String searchType = null;
+
+    private String sortType = null;
+
+    private String sortBy = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +68,6 @@ public class ContinentDetailActivity extends AppCompatActivity {
         continent = getIntent().getStringExtra("continent_name");
 
         binding.toolbar.setOnClickListener(v-> onBackPressed());
-        anyChartView = findViewById(R.id.continentPieChart);
 
         setContinentDetail();
     }
@@ -142,21 +151,6 @@ public class ContinentDetailActivity extends AppCompatActivity {
                     setPercentText(binding.percentContinentActiveCases, mPercentActiveCase);
                     setPercentText(binding.percentContinentDeath, mPercentDeathCase);
                     setPercentText(binding.percentContinentRecovered, mPercentRecoveredCase);
-
-                    double scaledActiveCase = ((double) activeCase / 1000);
-                    double scaledDeathCase = ((double) deathCase / 1000);
-                    double scaledRecoveredCase = ((double) recoveredCase / 1000);
-
-                    Pie pie = AnyChart.pie();
-
-                    List<DataEntry> dataEntryList = new ArrayList<>();
-                    dataEntryList.add(new ValueDataEntry("Active Case", scaledActiveCase));
-                    dataEntryList.add(new ValueDataEntry("Recovered Case", scaledRecoveredCase));
-                    dataEntryList.add(new ValueDataEntry("Death Case", scaledDeathCase));
-
-                    pie.data(dataEntryList);
-                    pie.title(response.body().getContinent() + " Continent Case");
-                    anyChartView.setChart(pie);
                 }
             }
 
@@ -185,5 +179,203 @@ public class ContinentDetailActivity extends AppCompatActivity {
 
     private void setPercentText(TextView tv, String textValue){
         tv.setText(HtmlCompat.fromHtml("<b>" + textValue + "</b> %", HtmlCompat.FROM_HTML_MODE_LEGACY));
+    }
+
+    private void dialogSearch() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_search, null);
+
+        EditText inputSearch = v.findViewById(R.id.textSearchBy);
+        Button btnSearch = v.findViewById(R.id.btnSearch);
+        RadioGroup radioSearchType = v.findViewById(R.id.radioGroupSearchType);
+        RadioButton radioSearchContinents = v.findViewById(R.id.radioSearchContinents);
+        RadioButton radioSearchCountries = v.findViewById(R.id.radioSearchCountries);
+
+        builder.setView(v);
+
+        AlertDialog dialogSearch = builder.create();
+        if (dialogSearch.getWindow() != null) {
+            dialogSearch.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+
+            radioSearchType.setOnCheckedChangeListener((group, checkedId) -> {
+                if(checkedId == R.id.radioSearchContinents){
+                    searchType = radioSearchContinents.getText().toString();
+                } else if (checkedId == R.id.radioSearchCountries){
+                    searchType = radioSearchCountries.getText().toString();
+                }
+            });
+
+            btnSearch.setOnClickListener(view -> doSearch(inputSearch.getText().toString()));
+
+            inputSearch.setOnEditorActionListener((v1, actionid, event) -> {
+                if (actionid == EditorInfo.IME_ACTION_GO) {
+                    doSearch(inputSearch.getText().toString());
+                }
+                return false;
+            });
+            dialogSearch.show();
+        }
+    }
+
+    private void doSearch(String query) {
+        if(query.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Tidak ada inputan!!!", Toast.LENGTH_SHORT).show();
+        }
+
+        if(searchType.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Harap pilih tipe search!!!", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (searchType.equals("Continents")){
+            Intent i = new Intent(ContinentDetailActivity.this, SearchActivity.class);
+            i.putExtra("searchType", "continents");
+            i.putExtra("searchQuery", query);
+            startActivity(i);
+        }
+
+        else if (searchType.equals("Countries")){
+            Intent i = new Intent(ContinentDetailActivity.this, SearchActivity.class);
+            i.putExtra("searchType", "countries");
+            i.putExtra("searchQuery", query);
+            startActivity(i);
+        }
+    }
+
+    private void dialogSort() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_sort, null);
+
+        Button btnSearch = v.findViewById(R.id.btnSort);
+        RadioGroup radioSortGroupType = v.findViewById(R.id.radioGroupSortType);
+        RadioButton radioSortContinents = v.findViewById(R.id.radioButtonSortContinents);
+        RadioButton radioSortCountries = v.findViewById(R.id.radioButtonSortCountries);
+
+        RadioGroup radioSortGroupBy1 = v.findViewById(R.id.radioGroup1SortBy);
+        RadioButton radioSortByTotal = v.findViewById(R.id.radioButtonSortTotal);
+        RadioButton radioSortByActive = v.findViewById(R.id.radioButtonSortActive);
+
+        RadioGroup radioSortGroupBy2 = v.findViewById(R.id.radioGroup2SortBy);
+        RadioButton radioSortByRecovered = v.findViewById(R.id.radioButtonSortRecovered);
+        RadioButton radioSortByDeath = v.findViewById(R.id.radioButtonSortDeaths);
+
+        builder.setView(v);
+
+        AlertDialog dialogSort = builder.create();
+        if (dialogSort.getWindow() != null) {
+            dialogSort.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            radioSortGroupType.setOnCheckedChangeListener((group, checkedId) -> {
+                if(checkedId == R.id.radioButtonSortContinents){
+                    sortType = radioSortContinents.getText().toString().toLowerCase();
+                } else if (checkedId == R.id.radioButtonSortCountries){
+                    sortType = radioSortCountries.getText().toString().toLowerCase();
+                }
+            });
+
+            radioSortGroupBy1.setOnCheckedChangeListener((group, checkedId) -> {
+                if(checkedId == R.id.radioButtonSortTotal){
+                    radioSortGroupBy2.setOnCheckedChangeListener(null);
+                    sortBy = radioSortByTotal.getText().toString().toLowerCase().replace("total case", "cases");
+                } else if (checkedId == R.id.radioButtonSortActive) {
+                    radioSortGroupBy2.setOnCheckedChangeListener(null);
+                    sortBy = radioSortByActive.getText().toString().toLowerCase().replace("active case", "actives");
+                }
+            });
+
+            radioSortGroupBy2.setOnCheckedChangeListener((group, checkedId) -> {
+                if (checkedId == R.id.radioButtonSortRecovered){
+                    radioSortGroupBy1.setOnCheckedChangeListener(null);
+                    sortBy = radioSortByRecovered.getText().toString().toLowerCase();
+                } else if (checkedId == R.id.radioButtonSortDeaths){
+                    radioSortGroupBy1.setOnCheckedChangeListener(null);
+                    sortBy = radioSortByDeath.getText().toString().toLowerCase().replace("death", "deaths");
+                }
+            });
+
+            btnSearch.setOnClickListener(view -> doSort(sortBy));
+            dialogSort.show();
+        }
+    }
+
+    private void doSort(String query) {
+        if(query.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Harap pilih tipe continents / countries!!!", Toast.LENGTH_SHORT).show();
+        }
+        if(sortType.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Harap pilih jenis sorting data!!!", Toast.LENGTH_SHORT).show();
+        } else if (sortType.equals("continents")){
+            switch (query) {
+                case "cases":
+                    Intent sortContinentsByCase = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortContinentsByCase.putExtra("sortType", "continents");
+                    sortContinentsByCase.putExtra("sortBy", "cases");
+                    startActivity(sortContinentsByCase);
+                    break;
+                case "actives":
+                    Intent sortContinentsByActive = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortContinentsByActive.putExtra("sortType", "continents");
+                    sortContinentsByActive.putExtra("sortBy", "actives");
+                    startActivity(sortContinentsByActive);
+                    break;
+                case "recovered":
+                    Intent sortContinentsByRecovered = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortContinentsByRecovered.putExtra("sortType", "continents");
+                    sortContinentsByRecovered.putExtra("sortBy", "recovered");
+                    startActivity(sortContinentsByRecovered);
+                    break;
+                case "deaths":
+                    Intent sortContinentsByDeath = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortContinentsByDeath.putExtra("sortType", "continents");
+                    sortContinentsByDeath.putExtra("sortBy", "deaths");
+                    startActivity(sortContinentsByDeath);
+                    break;
+            }
+
+        } else if (sortType.equals("countries")){
+            switch (query) {
+                case "cases":
+                    Intent sortCountryByCase = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortCountryByCase.putExtra("sortType", "countries");
+                    sortCountryByCase.putExtra("sortBy", "cases");
+                    startActivity(sortCountryByCase);
+                    break;
+                case "actives":
+                    Intent sortCountryByActive = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortCountryByActive.putExtra("sortType", "countries");
+                    sortCountryByActive.putExtra("sortBy", "actives");
+                    startActivity(sortCountryByActive);
+                    break;
+                case "recovered":
+                    Intent sortCountryByRecovered = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortCountryByRecovered.putExtra("sortType", "countries");
+                    sortCountryByRecovered.putExtra("sortBy", "recovered");
+                    startActivity(sortCountryByRecovered);
+                    break;
+                case "deaths":
+                    Intent sortCountryByDeath = new Intent(ContinentDetailActivity.this, SortActivity.class);
+                    sortCountryByDeath.putExtra("sortType", "countries");
+                    sortCountryByDeath.putExtra("sortBy", "deaths");
+                    startActivity(sortCountryByDeath);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.nav_search) {
+            dialogSearch();
+        } else if (item.getItemId() == R.id.nav_sort){
+            dialogSort();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
